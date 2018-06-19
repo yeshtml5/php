7a38de8985641cb071e589b3ff34f9953cc11b5e
yeshtml5 <yeshtml5@gmail.com>
2018-06-20 00:49:23 +0900

    study

--- memo/index.php
+++ memo/index.php
@@ -1,40 +1,45 @@
 <?php
-    /*
-    $file = new SplFileObject('data.txt');
-    $file->fwrite((rand(1, 100)));
-    var_dump($file->fread($file->getSize()))
-    */
+/*
+$file = new SplFileObject('data.txt');
+$file->fwrite((rand(1, 100)));
+var_dump($file->fread($file->getSize()))
+*/
 ?>
-
-
 <?php
-    /*
-     * FileMemo : class
-     */
-    class FileMemo {
-        private $filename;
+class FileMemo {
+    private $fileRead;
+    private $filename;
+    public function __construct($file_name) {
+        $this->filename = $file_name;
+        //유효성체크
+        $this->ifEmptyDie($file_name);
+        $this->fileRead = file_get_contents($file_name);
 
-        function __construct($file_name) {
-            $this->filename = $file_name;
-            //유효성체크
-            if (!file_exists($this->filename)) {
-                die('There is no file' . $this->filename);
-            }
-        }
-
-        function isFile() {
-            return is_file($this->filename);
-        }
-
-        function getFile() {
-            return $this->filename;
+        $this->read();
+    }
+    function read() {
+       var_dump($this->fileRead );
+    }
+    function isFile() {
+        return is_file($this->filename);
+    }
+    function setFile($_data) {
+        $this->ifEmptyDie($_data);
+        $this->filename = $_data;
+    }
+    function getFile() {
+        return $this->filename;
+    }
+    /*private*/
+    private function ifEmptyDie($_value) {
+        if (!file_exists($_value)) {
+            die('There is no file or params ' . $_value);
         }
     }
-    /*----[instance]*/
-    $file = new FileMemo('data.txt');
-    var_dump($file->isFile());
-    var_dump($file->getFile());
-    echo "<p></p>";
+}
+/*----[instance]*/
+$file = new FileMemo('data.txt');
+var_dump($file->getFile());
 ?>
 <article>
     <hr>
