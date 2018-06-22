3e0945b9b8a806e458b996d044b443b5afab45fb
wanhwi.son <yeshtml5@gmail.com>
2018-06-22 18:08:05 +0900

    DB_connect

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/php/inc/common.inc"); ?>
+<?php
+    $query = "SELECT * FROM `wp_users` ";
+    $data = db_query($query);
+?>
 <?php head(); ?>
     <!--contents-->
     <article>
+        <pre>
+            <?php var_dump($data); ?>
+            <?php echo count($data); ?>
+        </pre>
         <ul>
             <li><a href="/php/memo/" target="_blank">메모장</a></li>
+            <li><a href="https://opentutorials.org/course/3167/19600" target="_blank">https://opentutorials.org/course/3167/19600</a>
+            </li>
             <li><a href="https://opentutorials.org/course/3130" target="_blank">생활코딩 PHP</a></li>
         </ul>
         <h1>참고파일</h1>
