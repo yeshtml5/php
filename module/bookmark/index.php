a711a852b7dacee233ad1fd18d023669f7594775
wanhwi.son <yeshtml5@gmail.com>
2018-06-28 16:44:29 +0900

    삭제및 저장

--- module/bookmark/index.php
+++ module/bookmark/index.php
@@ -11,14 +11,15 @@
     }
     /*------[logic]--------*/
     if (isset($_POST['submit']) && $_POST['submit'] == "INSERT"):/*------[INSERT]--------*/
-        $sql = "INSERT INTO $DB_TABLE (`id`,`memo`,`url`,`date`,`favorite`) VALUES (null, '{$_POST['memo']}', '{$_POST['url']}', '', 'N')";
+        $now = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO $DB_TABLE (`id`,`memo`,`url`,`date`,`favorite`) VALUES (null, '{$_POST['memo']}', '{$_POST['url']}', '{$now}', 'N')";
         $result = db_query($sql);
         reload();
     elseif (isset($_POST['submit']) && $_POST['submit'] == "DELETE"):/*------[DELETE]--------*/
         $sql = "DELETE FROM $DB_TABLE WHERE `php_bookmark`.`id` = '{$_POST['row_id']}'";
         $result = db_query($sql);
         reload();
-    elseif (isset($_POST['submit']) && $_POST['submit'] == "MODIFY"):/*------[UPDATE]--------*/
+    elseif (isset($_POST['submit']) && $_POST['submit'] == "MODIFY"):/*------[MODIFY]--------*/
         //---
         $submit_label = 'UPDATE';
         $sql = "SELECT * FROM $DB_TABLE WHERE id={$_POST['row_id']}";
@@ -26,8 +27,10 @@
         $row = mysqli_fetch_array($result);
         $escaped['memo'] = htmlspecialchars($row['memo']);
         $escaped['url'] = htmlspecialchars($row['url']);
+        $escaped['favorite'] = htmlspecialchars($row['favorite']);
+        var_dump($escaped['favorite']);
     elseif (isset($_POST['submit']) && $_POST['submit'] == "UPDATE"):/*------[UPDATE]--------*/
-        $sql = "UPDATE $DB_TABLE SET `memo` = '{$_POST['memo']}', `url` = '{$_POST['url']}' WHERE id='{$_POST['row_id']}'";
+        $sql = "UPDATE $DB_TABLE SET `memo` = '{$_POST['memo']}', `url` = '{$_POST['url']}',`favorite` = '{$_POST['row_checked']}' WHERE id='{$_POST['row_id']}'";
         $result = db_query($sql);
         reload();
     else:
@@ -37,46 +40,57 @@
     $sql = "SELECT * FROM $DB_TABLE";
     $result = db_query($sql);
 ?>
-<?php head(); ?>
+<!Doctype html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <!--[script]-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+    <!--[style]-->
+    <link type="text/css" rel="stylesheet" href="<?php theme(); ?>src/css/fontawesome.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php theme(); ?>src/css/basic.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php theme(); ?>src/css/common.css"/>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+</head>
+<body>
 <!--contents-->
 <article>
-    <section>
-        <div class="bookmark">
-            <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
-                <input type="hidden" name="row_id" value="<?= $row['id']; ?>">
-                <input type="text" name="memo" placeholder="memo" value="<?= $escaped['memo']; ?>">
-                <input type="text" name="url" placeholder="url" value="<?= $escaped['url']; ?>">
-                <input type="submit" name="submit" value="<?= $submit_label; ?>">
-            </form>
-            <?php while ($row = mysqli_fetch_array($result)): ?>
-                <dl>
-                    <dt><input type="checkbox" <?php if ($row['favorite'] == "Y") {
-                            echo "checked = 'checked'";
-                        } ?> ></dt>
-                    <dd>
-                        <a href="<?= $row['url']; ?>" target="_blank"><?= $row['memo']; ?></a>
-                    </dd>
-                    <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
+    <section class="bookmark-wrap">
+        <div class="cont">
+            <div class="create-wrap">
+                <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
+                    <?php if (isset($row)) : ?>
+                        <input type="hidden" name="row_id" value="<?= $row['id']; ?>">
+                        <input type="checkbox" name="row_checked" <?php if ($row['favorite'] == "on") {echo "checked = 'checked'";} ?> >
+                    <?php endif; ?>
+                    <input type="text" name="memo" placeholder="memo" value="<?= $escaped['memo']; ?>">
+                    <input type="text" name="url" placeholder="url" value="<?= $escaped['url']; ?>">
+                    <input type="submit" name="submit" value="<?= $submit_label; ?>">
+                </form>
+            </div>
+            <div class="list-wrap">
+                <?php while ($row = mysqli_fetch_array($result)): ?>
+                    <dl>
+                        <dt><input type="checkbox" name="checked"
+                                   value="<?= $row['favorite'] ?>" <?php if ($row['favorite'] == "on") {
+                                echo "checked = 'checked'";
+                            } ?> ></dt>
                         <dd>
-                            <input type="hidden" name="row_id" value="<?= $row['id']; ?>">
-                            <input type="submit" name="submit" value="DELETE">
-                            <input type="submit" name="submit" value="MODIFY">
-
-                            <?= $row['id']; ?>
+                            <a href="<?= $row['url']; ?>" target="_blank"><?= $row['memo']; ?></a>
                         </dd>
-                    </form>
-                </dl>
-            <?php endwhile; ?>
+                        <dd class="btn-wrap">
+                            <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
+                                <input type="hidden" name="row_id" value="<?= $row['id']; ?>">
+                                <input type="submit" name="submit" value="DELETE">
+                                <input type="submit" name="submit" value="MODIFY">
+                            </form>
+                        </dd>
+                    </dl>
+                <?php endwhile; ?>
+            </div>
         </div>
     </section>
 </article>
 <!--//contents-->
-<?php footer(); ?>
-<style>
-    form input[type="text"] {display:inline-block;padding:10px 20px; border:1px solid #000;}
-    section {display:block;width:100%;padding:50px;text-align:left;}
-    .bookmark {display:inline-block;width:500px;margin:0 auto;}
-    .bookmark dl {border-bottom:1px solid #111;}
-    .bookmark dl > * {display:inline-block;padding:10px;}
-    .bookmark dl dd a:hover {color:#ff0000;}
-</style>
+</body>
+</html>
