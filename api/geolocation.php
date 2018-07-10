c9e5598805b6c2793ff565d8ccbaf4fce660e38f
wanhwi.son <yeshtml5@gmail.com>
2018-07-10 17:16:05 +0900

    time api 개발

--- api/geolocation.php
+++ api/geolocation.php
@@ -38,7 +38,7 @@
                     output.innerHTML = '<p>위도 : ' + latitude + '° <br>경도 : ' + longitude + '°</p>';
 
                     var img = new Image(600, 600);
-                    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=500x500&sensor=false";
+                    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=500x500&sensor=false";
                     output.appendChild(img);
                     window.location.href = "?mode=false&lat="+latitude+"&lon="+longitude;
 
