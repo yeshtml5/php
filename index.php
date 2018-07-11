603010ab8d210507bbedcc526009e0bfdd520dcb
wanhwi.son <yeshtml5@gmail.com>
2018-07-11 14:55:26 +0900

    menu -> $_GET

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <div class="panel">
             <?php
                 if (isset($_GET['inc'])):
-                    include (__ROOT__ . $_GET['inc']);
+                    include(__ROOT__ . $_GET['inc']);
                 else:
                     $files = array_diff(scandir("./"), array(".", "..", "db", ".git", ".idea", ".gitignore", "README.md"));
                     foreach ($files as &$key) {
@@ -14,10 +14,24 @@
                 endif;
             ?>
         </div>
+
         <div class="mgt_50 mgb_50 panel">
+            <img src="https://vuejs.org/images/logo.png" style="width:100px;">
+            <p id="app">{{message}}</p>
             <?php
             ?>
         </div>
     </article>
     <!--//contents-->
+    <script type="text/javascript">
+        var app = new Vue({
+            el: '#app',
+            data: {
+                message: 'Hellow world'
+            }
+        })
+        setTimeout(function () {
+            app.message = "TEST";
+        }, 1000);
+    </script>
 <?= footer(); ?>
